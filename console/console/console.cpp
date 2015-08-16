7f22000d2160006b196bd8a7f733f7d82aa726f9
thiago <thiago.adams@gmail.com>
2015-08-16 17:02:41 -0300

    Sample project

---
+++ console/console/console.cpp
@@ -0,0 +1,13 @@
+// console.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "conio.h"
+
+int main()
+{
+    clrscr();
+    textcolor(BLUE);
+    return 0;
+}
+
